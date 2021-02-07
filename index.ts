738be7c56b165a877116c7cfe4652bfec3f2dd33
Ismael <biv615912@udelasalle.edu.mx>
2021-02-06 22:50:51 -0600

    Inicial

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response, NextFunction} from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.get('/',(req: Request, res: Response)=>{
+    res.status(200).json({
+        ok: true,
+        msg: `API NODEJS funcionando OK`
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('Servidor express corriendo en pto 3000');
+});
